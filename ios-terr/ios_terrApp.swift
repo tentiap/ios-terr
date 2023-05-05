cfdbd356e00b41cb0c70318f6b7fa1453723bfa7
tentiap <inooyabu@gmail.com>
2023-05-05 12:07:47 +0700

    Initial Commit

---
+++ ios-terr/ios_terrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_terrApp.swift
+//  ios-terr
+//
+//  Created by Tenti Atika Putri on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_terrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
